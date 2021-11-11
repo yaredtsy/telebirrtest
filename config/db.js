e0f2f06fdf4fc0c6f4dfbd3a5210fa232ac8f045
yaredtsy <34584266+yaredtsy@users.noreply.github.com>
2021-11-11 00:01:09 -0800

    test

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURILocal');
+const db = config.get('mongoURI');
 const { MongoClient } = require('mongodb');
 const uri = db;
 
